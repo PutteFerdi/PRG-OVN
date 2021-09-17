2f77f60edbe928d303d928f40481a42a1e890e0d
PutteFerdi <90191269+PutteFerdi@users.noreply.github.com>
2021-09-17 13:34:56 +0200

    🎉 uttryck

---
+++ uttryck.py
@@ -0,0 +1,13 @@
+tal1 = input ("skriv ett heltal ")
+tal2 = input ("skriv ett tal till ")
+summa = int(tal1) + float(tal2)
+
+print(f"summan av dom dom trvå taler är {summa}")
+
+tal1 = 12
+tal2 = 24
+
+summa = tal1 + tal1
+
+print(tal1 == summa)
+print(tal2 == summa)
